8e2392f7097cde7241ae452bf6aad9370116abd3
MegaShinySunshine <oliolinochka@gmail.com>
2026-03-01 13:38:07 +0300

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ export function Footer() {
   return (
     <footer className="py-16 text-center">
       <p className="text-sm text-slate-400">
-        Спасибо за внимание •{" "}
-        <span className="text-slate-500 font-medium">Матвеенко А.Р.</span>
+        История возникновения денег{" "}
+        <span className="text-slate-500 font-medium"></span>
       </p>
     </footer>
   );
