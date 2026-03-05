989aaba3e5cbebab1b1fe20a5a419c901bcc2ccb
MegaShinySunshine <oliolinochka@gmail.com>
2026-03-06 01:05:41 +0300

    fix: modules imports

---
+++ src/vite-env.d.ts
@@ -0,0 +1,31 @@
+// Объявление модулей для картинок
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.svg" {
+    const value: string;
+    export default value;
+}
+
+// Объявление модулей для аудио
+declare module "*.mp3" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.wav" {
+    const value: string;
+    export default value;
+}
